c2c0794e40f4ea6a6e9a7a34ffd3e20b9904441a
CoulsonP <coulsonp@icloud.com>
2023-01-23 10:48:48 -0600

    updated comments and added tag routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  //new category
   Category.create(req.body)
   .then(data => res.status(200).json(data))
   .catch(err => res.status(400).json(err))
